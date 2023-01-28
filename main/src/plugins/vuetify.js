022035ce5249ea8c51509f0ea1f81399b861d8b4
TakumaSatke <gold24onmyheart@gmail.com>
2023-01-28 16:05:18 +0900

    サンプルのメインフレーム作成

---
+++ main/src/plugins/vuetify.js
@@ -0,0 +1,35 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+// テーマカラーのカスタム
+// const myCustomLightTheme = {
+//   dark: false,
+//   colors: {
+//     background: '#FFFFFF',
+//     surface: '#FFFFFF',
+//     primary: '#6200EE',
+//     'primary-darken-1': '#3700B3',
+//     secondary: '#03DAC6',
+//     'secondary-darken-1': '#018786',
+//     error: '#B00020',
+//     info: '#2196F3',
+//     success: '#4CAF50',
+//     warning: '#FB8C00',
+//   }
+// }
+
+export default createVuetify({
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+
+  // テーマ設定
+  // theme: {
+  //   defaultTheme: 'myCustomLightTheme',
+  //   themes: {
+  //     myCustomLightTheme,
+  //   }
+  // }
+})
